1189faf242928c6ebe417b7fd6f1244f3f5aa12d
Rida <ridaox119@gmail.com>
2025-10-25 05:58:00 +0100

    Inplementhero section

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // يمكنك تبدلو إذا بغيتي
+    open: true  // كيفتح المتصفح تلقائياً
+  }
 })
+
